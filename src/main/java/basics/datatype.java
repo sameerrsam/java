1c7b767363c1815d28606568b061c59353ace272
sameerrsam <sameerrsam007@gmail.com>
2024-10-16 15:29:25 +0530

    good

---
+++ src/main/java/basics/datatype.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class datatype {
+    public static void main(String[] args) {
+        int biriyani = 450;
+        float raita = 21.5f;
+        String name = "food";
+        char initial= 'A';
+        double amount= 500.55;
+        System.out.println("integer value"+biriyani);
+        System.out.println("float value:"+raita);
+        System.out.println("string value:"+name);
+        System.out.println("character value:"+initial);
+        System.out.println("double value:"+amount);
+
+    }
+}
---
+++ src/main/java/basics/variables.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class variables {
+    public static void main(String[] args) {
+        int value=50;
+        int value1=100;
+        int result=value+value1;
+        System.out.println("the output is:"+result);
+        System.out.println(result);
+        System.out.println("result");
+    }
+}
